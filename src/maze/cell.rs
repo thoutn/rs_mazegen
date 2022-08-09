87652bda6a60c2ff69ca8c4f92bc9584752be910
Tamas Nagy <tamasn.nagy004@gmail.com>
2022-08-09 17:45:28 +0200

    Added smart pointers to enable cyclic types

--- src/maze/cell.rs
+++ src/maze/cell.rs
@@ -1,36 +1,39 @@
-use core::hash::Hash;
-use std::cmp::{ PartialEq, Eq };
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+use std::cmp::{PartialEq, Eq};
 
 
-pub trait Same: PartialEq + Eq + Hash {}
+type CellWeakLink = Weak<RefCell<Cell>>;
+type CellStrongLink = Rc<RefCell<Cell>>;
 
 
-#[derive(PartialEq, Eq, Hash)]
-pub struct Cell<'a, T>
-where T: Same
-{
+// #[derive(PartialEq, Eq)]
+pub struct Cell {
     pub row: u16,
     pub col: u16,
 
-    links: Vec<&'a T>,
+    links: Vec<CellWeakLink>,
 
-    pub top: Option<&'a T>,
-    pub bottom: Option<&'a T>,
-    pub left: Option<&'a T>,
-    pub right: Option<&'a T>,
+    pub top: Option<CellWeakLink>,
+    pub bottom: Option<CellWeakLink>,
+    pub left: Option<CellWeakLink>,
+    pub right: Option<CellWeakLink>,
+}
+
+impl PartialEq for Cell {
+    fn eq(&self, other: &Self) -> bool {
+        self.row == other.row && self.col == other.col
+    }
 }
 
 
-impl<'a, T: Same> Same for Cell<'a, T> {}
-
-
-impl<'a, T: Same> Cell<'a, T> {
+impl Cell {
     pub fn new(row: u16, col: u16) -> Self {
         Self {
             row,
             col,
 
-            links: Vec::new(),
+            links: vec![],
 
             top: None,
             bottom: None,
@@ -40,8 +43,12 @@ impl<'a, T: Same> Cell<'a, T> {
     }
 
 
-    pub fn link_to(&mut self, cell: &'a mut T) {
-        self.links.push(cell);
+    pub fn link_to(&mut self, cell: CellWeakLink, _bidirect: Option<bool>) {
+        self.links.push(Rc::downgrade(&cell.upgrade().unwrap()));
+        // if _bidirect.unwrap_or(true) {
+        //     let mut cell = (&*cell.upgrade().unwrap()).borrow_mut();
+        //     cell.link_to(Rc::downgrade(&Rc::new(RefCell::new(&*self))), Some(false));
+        // }
     }
 
 
@@ -50,18 +57,34 @@ impl<'a, T: Same> Cell<'a, T> {
     }
 
 
-    pub fn is_linked_to(&self, cell: &T) -> bool {
-        self.links.contains(&cell)
+    pub fn is_linked_to(&self, cell: CellWeakLink) -> bool {
+        // self.links.contains(&cell) // PartialEq not implemented on Weak<RefCell<Cell>>
+
+        for link in &self.links {
+            if *(&*link.upgrade().unwrap()).borrow() == *(&*cell.upgrade().unwrap()).borrow() {
+                return true;
+            }
+        }
+
+        false
     }
 
 
-    pub fn neighbours(&self) -> Vec<Option<&T>> {
-        let mut lst: Vec<Option<&T>> = Vec::new();
+    pub fn neighbours(&self) -> Vec<CellWeakLink> {
+        let mut lst: Vec<CellWeakLink> = vec![];
 
-        if self.top.is_some() { lst.push(self.top); }
-        if self.bottom.is_some() { lst.push(self.bottom); }
-        if self.left.is_some() { lst.push(self.left); }
-        if self.right.is_some() { lst.push(self.right); }
+        if let Some(cell) = &self.top {
+            lst.push(Rc::downgrade(&cell.upgrade().unwrap()));
+        }
+        if let Some(cell) = &self.bottom {
+            lst.push(Rc::downgrade(&cell.upgrade().unwrap()));
+        }
+        if let Some(cell) = &self.left {
+            lst.push(Rc::downgrade(&cell.upgrade().unwrap()));
+        }
+        if let Some(cell) = &self.right {
+            lst.push(Rc::downgrade(&cell.upgrade().unwrap()));
+        }
 
         lst
 
@@ -74,26 +97,60 @@ impl<'a, T: Same> Cell<'a, T> {
 mod tests {
     use super::*;
 
+
     #[test]
     fn test_link_to() {
-        let mut cell1 = Cell::new(2, 1);
-        let mut cell2 = Cell::new(2, 2);
+        let cell1 = Rc::new(RefCell::new(Cell::new(2, 1)));
+        let cell2 = Rc::new(RefCell::new(Cell::new(2, 2)));
 
-        cell1.link_to(&mut cell2);
-        cell2.link_to(&mut cell1); //cyclic type of infinite size
+        (&*cell1).borrow_mut().link_to(Rc::downgrade(&cell2), Some(true));
+        (&*cell2).borrow_mut().link_to(Rc::downgrade(&cell1), Some(true));
 
-        let cell = *cell1.links.get(0).unwrap();
-        assert_eq!(2, cell.row);
+        // println!("{}", (&*cell1).borrow().row);
+        // println!("{}", RefCell::borrow(&cell1).row);
+
+        assert_eq!(true, (&*cell1).borrow().is_linked_to(Rc::downgrade(&cell2)));
     }
+
+
+    #[test]
+    fn test_is_not_linked_to() {
+        let cell1 = Rc::new(RefCell::new(Cell::new(2, 1)));
+        let cell2 = Rc::new(RefCell::new(Cell::new(2, 2)));
+        let cell3 = Rc::new(RefCell::new(Cell::new(3, 1)));
+
+        (&*cell1).borrow_mut().link_to(Rc::downgrade(&cell2), Some(true));
+        (&*cell2).borrow_mut().link_to(Rc::downgrade(&cell1), Some(true));
+
+        assert_eq!(false, (&*cell1).borrow().is_linked_to(Rc::downgrade(&cell3)));
+    }
+
 
     #[test]
     fn test_right_left() {
-        let mut cell1 = Cell::new(2, 1);
-        let mut cell2 = Cell::new(2, 2);
+        let cell1 = Rc::new(RefCell::new(Cell::new(2, 1)));
+        let cell2 = Rc::new(RefCell::new(Cell::new(2, 2)));
 
-        cell1.right = Some(&cell2);
-        cell2.left = Some(&cell1); //cyclic type of infinite size
+        (&*cell1).borrow_mut().right = Some(Rc::downgrade(&cell2));
+        (&*cell2).borrow_mut().left = Some(Rc::downgrade(&cell1));
 
-        assert_eq!(1, (*cell2.left.unwrap()).col);
+        let cell = (&*cell2).borrow();
+        assert_eq!(true, cell.left.is_some());
+    }
+
+
+    #[test]
+    fn test_neighbours() {
+        let cell1 = Rc::new(RefCell::new(Cell::new(2, 1)));
+        let cell2 = Rc::new(RefCell::new(Cell::new(2, 2)));
+        let cell3 = Rc::new(RefCell::new(Cell::new(3, 1)));
+
+        (&*cell1).borrow_mut().right = Some(Rc::downgrade(&cell2));
+        (&*cell1).borrow_mut().top = Some(Rc::downgrade(&cell3));
+        (&*cell2).borrow_mut().left = Some(Rc::downgrade(&cell1));
+
+        let neighbours = (&*cell1).borrow().neighbours();
+
+        assert_eq!(true, !neighbours.is_empty());
     }
 }
